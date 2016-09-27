e96aa25dca52928c9252cab2e0aab271ac2ad848
郭乐乐 <lel.guo@haihangyun.com>
2016-09-27 17:36:37 +0800

    Initial Commit

---
+++ StreamDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StreamDemo
+//
+//  Created by __无邪_ on 16/9/27.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StreamDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StreamDemo
+//
+//  Created by __无邪_ on 16/9/27.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
